311e9ef27ee62494c83033ee4ed7b385bc65f671
The flying pigeon <57905673+theflyingpigeon@users.noreply.github.com>
2021-05-23 01:54:02 +0200

    Added a confirm when ypu delete multiple items

--- client/src/components/mainpages/Footer/Footer.js
+++ client/src/components/mainpages/Footer/Footer.js
@@ -3,9 +3,11 @@ import {SocialIcon} from "react-social-icons";
 
 function Footer() {
     return (
-        <div style={{bottom: 0}}>
-            <SocialIcon url={"https://www.instagram.com/agorathu/"}/>
-            <p style={{color: "#555", marginTop: 5}}>KVK number: [insert KVK number]</p>
+        <div className={"footer"}>
+            <SocialIcon url={"https://www.instagram.com/agorathu/"} fgColor={"rgb(167,136,12)"} bgColor={"rgb(0,47,0)"}/>
+            <br />
+            <br />
+            <p>KVK number: [insert KVK number]</p>
         </div>
     );
 }
