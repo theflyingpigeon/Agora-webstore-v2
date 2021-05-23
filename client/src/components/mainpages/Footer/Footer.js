b083e9194b44e4631b464771d6486a209457e0db
The flying pigeon <57905673+theflyingpigeon@users.noreply.github.com>
2021-05-23 02:00:48 +0200

    Added a confirm when ypu delete multiple items

--- client/src/components/mainpages/Footer/Footer.js
+++ client/src/components/mainpages/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <div className={"footer"}>
             <SocialIcon url={"https://www.instagram.com/agorathu/"} fgColor={"rgb(167,136,12)"} bgColor={"rgb(0,47,0)"}/>
             <SocialIcon url={"https://www.youtube.com/channel/UCfelZ72mgtKvnuda0l_YyAQ"} fgColor={"rgb(167,136,12)"} bgColor={"rgb(0,47,0)"} style={{marginLeft: "1rem"}}/>
-            <SocialIcon url={"mailto:shaeme@icloud.com"} fgColor={"rgb(167,136,12)"} bgColor={"rgb(0,47,0)"} network={"email"} style={{marginLeft: "1rem"}}/>
+            <SocialIcon url={"mailto:"} fgColor={"rgb(167,136,12)"} bgColor={"rgb(0,47,0)"} network={"email"} style={{marginLeft: "1rem"}}/>
             <br />
             <br />
             <p>KVK number: [insert KVK number]</p>
